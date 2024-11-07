70aa4761d2a763384c7ce8dceb085bfd3c66946c
Saray Mendivelso <zaryz2005@gmail.com>
2024-11-07 06:09:06 -0500

    Proyecto Funcional

---
+++ src/main/java/org/eci/CorsConfig.java
@@ -0,0 +1,17 @@
+package org.eci;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:3000") // Cambia esto por la URL de tu frontend si es diferente
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
